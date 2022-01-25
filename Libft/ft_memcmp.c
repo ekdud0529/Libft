bd7b799311f77a25e1d05290ad6d08c135dc7758
ekdud0529 <ekffkddl0529@naver.com>
2022-01-25 20:37:02 +0900

    modify memcmp type

--- Libft/ft_memcmp.c
+++ Libft/ft_memcmp.c
@@ -17,8 +17,8 @@ int	ft_memcmp(const void *str1, const void *str2, size_t n)
 	unsigned char	*s1;
 	unsigned char	*s2;
 
-	s1 = str1;
-	s2 = str2;
+	s1 = (unsigned char *)str1;
+	s2 = (unsigned char *)str2;
 	while (n--)
 	{
 		if (*s1 != *s2)
